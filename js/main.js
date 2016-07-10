9ca33d19cb3c61d478b6674ad754a7de763f8115
lijin22 <lijin22@staff.sina.com.cn>
2016-07-10 11:23:08 +0800

    Site updated: 2016-07-10 11:23:08

--- js/main.js
+++ js/main.js
@@ -141,12 +141,12 @@ require([], function (){
     }
 
     // // Random Color 边栏顶部随机颜色
-    // // var colorList = ["#6da336", "#ff945c", "#66CC66", "#99CC99", "#CC6666", "#76becc", "#c99979", "#918597", "#4d4d4d"];
-    // var colorList = ["#4d4d4d"];
+    // var colorList = ["#6da336", "#ff945c", "#66CC66", "#99CC99", "#CC6666", "#76becc", "#c99979", "#918597", "#4d4d4d"];
+    // var colorList = ["#3f3f3f"];
     // var id = Math.ceil(Math.random()*(colorList.length-1));
     // // PC
     // $("#container .left-col .overlay").css({"background": colorList[id],"opacity": 1.0});
-    // // Mobile
+    // Mobile
     // $("#container #mobile-nav .overlay").css({"background-color": colorList[id],"opacity": 1.0});
 
     // Table
